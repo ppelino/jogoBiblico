d790b4591fbadc608294fc6c075ec608abe83ab0
Edson Gomes <78184175+ppelino@users.noreply.github.com>
2022-05-25 18:13:34 -0300

    Add files via upload

---
+++ Jogo Bliblico.py
@@ -0,0 +1,53 @@
+
+# *** JOGO BIBLICO *** Algoritmo de Abraao
+# (Genesis 18:24-32)
+# By Guilherme Jose Ferreira (leodmx@brturbo.com.br)
+
+# Inicio do programa
+print '\n'*100
+print """
+# *** JOGO BIBLICO *** Algoritmo de Abraao
+# By Guilherme Jose Ferreira (leodmx@brturbo.com.br)
+
+    Neste jogo voce deve convencer a Deus a nao destruir
+    Sodoma e Gomorra (Genesis 18:24-32). Algo um tanto
+    quanto muito dificil, mas vamos la:
+
+    No prompt "Eu:" Digite:
+    --> Senhor, e se houver xyz justos na cidade?
+    (Onde 'xyz' corresponde a um numero entre 0 e 999)
+
+    Lembre-se: Digite certo para acabar logo!
+
+    Boa sorte!!!
+"""
+raw_input('\nTecle <ENTER> ')
+
+# Inicio do jogo
+print '\n'*100
+numjust = 50
+while numjust >= 10:
+    justos = raw_input('Eu: ')
+    try:
+        if int(justos[20:23]) == numjust:
+            print "Deus: Nao destruirei a cidade por amor dos",numjust,"justos."
+            if numjust < 45:
+                numjust -= 5
+            numjust -= 5
+    # Jogo do tipo "quente ou frio"
+        elif int(justos[20:23]) > numjust:
+            print "Deus: Voce nao deveria pedir por menos justos?"
+        elif int(justos[20:23]) < numjust:
+            print "Deus: Voce nao gostaria de pedir por mais justos?"
+    # Se digitar errado, comeca tudo de novo
+    except ValueError:
+        print "Deus: Acaso vou destruir as cidades sem consultar Abraao?"
+        numjust = 50
+raw_input('\nTecle <ENTER> ')
+
+# Fim do jogo
+print '\n'*100
+print "\nDeus: Anjos, tirem Lo e sua familia de la..."
+print "\nAnjos: Sim, Senhor!"
+print "\n\n*** GAME OVER!!! ***\n"
+raw_input('\nTecle <ENTER> ')
